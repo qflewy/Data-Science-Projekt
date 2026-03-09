7ec13aae87a7af55801d3ec59cb325d54cd12e16
Bjarne <bjarne.j.bender@gmail.com>
2026-03-09 12:32:57 +0100

    DBSCAN implemented with testing Notebook for parameter selection (not fully commented!)

--- skripts/dbscanCompetitionDensity.py
+++ skripts/dbscanCompetitionDensity.py
@@ -7,9 +7,7 @@ import plotly.express as px
 
 
 def getCSVData(file_path):
-    
     df = pl.read_csv(file_path, null_values=["nicht", "NA", "N/A", "", "Nicht"])
-    df = df.select(["uuid", "latitude", "longitude"])
     print(df)
     return df
 
@@ -17,6 +15,9 @@ def performDBSCAN(data, eps, min_samples):
     cords = data.select(["latitude", "longitude"]).to_numpy()
     dbscan = DBSCAN(eps=eps, min_samples=min_samples)
     labels = dbscan.fit_predict(cords)
+    unique_labels = set(labels)
+    num_clusters = len(unique_labels) - (1 if -1 in unique_labels else 0)
+    print(f"Number of clusters: {num_clusters}")
     print(labels)
     return labels
 
@@ -28,11 +29,15 @@ def plotClusters(data, labels):
     # Plotly arbeitet einfacher mit pandas
     pdf = data.to_pandas()
 
+    # distinguish between noise (-1) and points in a cluster
+    pdf["status"] = np.where(pdf["cluster"] == -1, "noise", "clustered")
+
     fig = px.scatter_mapbox(
         pdf,
         lat="latitude",
         lon="longitude",
-        color="cluster",
+        color="status",
+        color_discrete_map={"noise": "red", "clustered": "green"},
         hover_name="uuid",
         zoom=5,
         center={"lat": 51.1657, "lon": 10.4515},  # Deutschland Mittelpunkt
@@ -40,8 +45,12 @@ def plotClusters(data, labels):
     )
 
     fig.update_layout(mapbox_style="open-street-map")
+    return fig
 
-    fig.show()
+
+def create_CSV_with_cluster_labels(data, labels, output_path):
+    data = data.with_columns(pl.Series("cluster", labels))
+    data.write_csv(output_path)
 
 
 df = getCSVData("C:\\Users\\Bjarne\\Desktop\\Uni\\Data Science Projekt\\PersonalTesting\\dbscan\\stations.csv")
